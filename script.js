0f847460c627a4c05e66fa18499d237615fcc471
Louise Onsoien <79151322+lonsoien@users.noreply.github.com>
2021-09-13 13:54:59 +0200

    java

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-const url = https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/3; 
+const url = "https://thronesapi.com/api/v2/Characters"; 
+ 
+const got = document.querySelector(".got");
 
+
+ async function gameOfthrones() {
+    const response = await fetch(url);
+    const gotCharacters = await response.json(); 
+
+    console.log(gotCharacters); 
+    
+    got.innerHTML = `<h1>${gotCharacters.fullName}</h1>
+                 <p>${gotCharacters.family}</p> 
+                 <div class="details-title">${gotCharacters.title}</div>`;  
+ }
+
+ gameOfthrones();
+
+ // <div class="character-image"
+ //style="background-image: url(`${gotCharacters.image}`)";></div>
